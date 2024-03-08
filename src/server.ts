f5d69b665d45a1b2f844d917b6fba2c0d725dc98
chandler <lcchrty@gmail.com>
2024-03-08 09:58:31 -0800

    server update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
 import formController from "./controller.ts"
 
+type ServerError = {
+	log: string,
+	status: number,
+	message: { err: string }
+}
+
 const app = express()
 
 
@@ -9,3 +15,16 @@ app.use(express.json())
 app.get('/api', formController.getResponses, (req, res,) => {
 	return res.status(200).json({ "hey": "hey" })
 })
+
+app.use('/', (err: ServerError, req: Request, res: Response, next: NextFunction) => {
+	const defaultErr: ServerError = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 500,
+		message: { err: 'An error occurred' },
+	};
+	const errorObj: ServerError = Object.assign({}, defaultErr, err);
+	console.log(errorObj.log);
+	return res.status(errorObj.status).json(errorObj.message);
+})
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
